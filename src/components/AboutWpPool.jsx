236ee1ff3d01b95803da5de51151ded6b126e6af
MimFSD <mimislam878@gmail.com>
2024-07-08 12:44:47 +0600

    About Section

--- src/components/AboutWpPool.jsx
+++ src/components/AboutWpPool.jsx
@@ -12,7 +12,9 @@ const AboutWpPool = () => {
                     <div className="lg:w-[628px] lg:pl-[72px] py-[78px]">
                         <h2 className='lg:text-[38px] text-[24px] text-[#191618] font-natsreguler'>About WPPOOL</h2>
                         <span className='lg:text-[30px] text-[20px] text-[#7D7F81] pt-[20px] font-natsreguler '>Investing in wppool for over 50 years</span>
+
                         <p className='pt-[16px] lg:text-[16px] text-[14px] lg:w-[622px] text-[#191618] font-messinabook'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,molestiae quas vel sint commodi  <u>repudiandae</u> consequuntur voluptatum laborumnumquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentiumoptio, eaque rerum! Provident similique accusantium nemo autem.</p>
+                        
                         <div className="lg:pt-[30px]  pt-[15px] lg:block flex justify-center">
                             <button className=' shadow-2xl py-[18px] px-[16px] rounded-[124px] bg-[#2042B6] text-[#FFF] font-messinareguler  hover:bg-[#fff] hover:border-[#2042B6] border-[1px] hover:text-[#2042B6] duration-300'>Contact WPPOOL</button>
                         </div>
